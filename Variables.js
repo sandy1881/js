bb3a3ecfee210dde9d36cbf60be60f99501edac8
Sandy <sandeshrnaik2000@gmail.com>
2024-12-13 04:28:59 +0530

    Variables

---
+++ Variables.js
@@ -0,0 +1,16 @@
+const accountId = 18;
+let accountEmail = "sandeshrnaik2000@gmail.com";
+var accountPassword ="00000";
+accountCity = "Banglore";
+let accountState; //undefined
+
+// accountId = 2;  //not allowed
+accountEmail = "sandeshhh";
+accountPassword = "1111"
+accountCity ="Bengaluru"
+/*
+Prefer not to use var 
+becouse oif issue in block scope and functional scope
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
