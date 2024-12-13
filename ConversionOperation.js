a1d5934aa2f6d502926a0e70b49022df963ad67f
Sandy <sandeshrnaik2000@gmail.com>
2024-12-13 16:55:21 +0530

    ConversionOperation

---
+++ ConversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33";
+console.log(typeof score);
+
+let valueInnumber = Number(score);
+console.log(typeof(valueInnumber));
+
+// "33" =>33
+// "33abc" =>NaN
+//true => 1
+
+let isLoggedIn =true;
+let booleanIsloggedIn =(Boolean(isLoggedIn));
+
+console.log(booleanIsloggedIn)
