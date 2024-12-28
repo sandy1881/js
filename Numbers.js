9da70d77923ae57f81871a38ac2b732472651d0d
Sandy <sandeshrnaik2000@gmail.com>
2024-12-28 23:06:21 +0530

    number

---
+++ Numbers.js
@@ -0,0 +1,15 @@
+let x = 123;  //to String method
+x.toString();
+(123).toString();
+(100 + 23).toString();
+
+Number(true);
+Number(false);
+Number("10");
+Number("  10");
+Number("10  ");
+Number(" 10  ");
+Number("10.33");
+Number("10,33");
+Number("10 33");
+Number("John");
